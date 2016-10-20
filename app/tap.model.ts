337d1731f047fef218ff37a44b1098016634c6f1
ian Wilcox <ian Wilcox>
2016-10-20 14:30:54 -0700

    add a tap/keg model

---
+++ app/tap.model.ts
@@ -0,0 +1,6 @@
+export class Tap {
+  public fill: number = 124;
+  constructor(public name: string, public brand: string, public price: number, public alcoholContent: number, public id: number) {   }
+}
+
+// import { Tap } from './tap.model';
